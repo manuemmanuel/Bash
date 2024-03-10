f522da6432fd46bc45c7a0351ed25385df435774
Manu Emmanuel <manuemmanuel2026@cs.sjcetpalai.ac.in>
2024-03-10 21:45:19 +0530

    Update fibonacci.sh

--- fibonacci.sh
+++ fibonacci.sh
@@ -1,11 +1,13 @@
-# Program to find the fibonacci series
+# Program to find the Fibonacci series
 echo "Enter the limit: "
 read limit
+a=0
+b=1
 echo -e "$a\n$b"
-for ((i=3;i<=limit;i++))
+for ((i=3; i<=limit; i++))
 do
-((c=$a+$b))
-a=$b
-b=$c
-echo "$c"
+    ((c=a+b))
+    a=$b
+    b=$c
+    echo "$c"
 done
