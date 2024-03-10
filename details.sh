446082e31057faed7a2a2d77ec6e7357b882c5cd
Manu Emmanuel <manuemmanuel2026@cs.sjcetpalai.ac.in>
2024-03-10 21:23:37 +0530

    Create details.sh

---
+++ details.sh
@@ -0,0 +1,9 @@
+echo "Enter the name: "
+read name
+echo "Enter the semester: "
+read sem
+echo "Enter the batch: "
+read batch
+echo "Enter the register number: "
+read reg
+echo -e "Name is $name\nSemester is $sem\nBatch is $batch\nRegister Number is $reg"
